7770b5241809373edcce83f56660346492349f7a
pfilipov <peter.filipov@telerik.com>
2016-06-09 13:20:50 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,151 @@
+var application = require("application");
+var Observable = require("data/observable").Observable;
+
+var frames = require("ui/frame");
+var view = require("ui/core/view");
+
+var All = "All";
+var Design = "Design";
+var Marketing = "Marketing";
+var Dev = "Development";
+
+var mainMenuId = "mainMenuSideDrawer";
+
+var utcDotNetDateConverter = function (value, format) {
+    var dotNetSubstringIndex = 6;
+
+    var result = new Date(parseInt(value.substr(dotNetSubstringIndex)));
+
+    return result.toUTCString();
+};
+
+var utcDateConverter = function (value, format) {
+    var result = new Date(value);
+
+    return result.toUTCString();
+};
+
+global.formatString = function(value, replacements) {
+    var formatted = value;
+    for (var i = 0; i < replacements.length; i++) {
+        var regexp = new RegExp('\\{' + i + '\\}', 'gi');
+        formatted = formatted.replace(regexp, replacements[i]);
+    }
+
+    return formatted;
+};
+
+global.extendModelWithNatigation = function (viewModel, page) {
+    viewModel.currentPage = page;
+
+    viewModel.navigateToAllNews = function () {
+        viewModel.navigateToNews(All);
+    };
+
+    viewModel.navigateToDesignNews = function () {
+        viewModel.navigateToNews(Design);
+    };
+
+    viewModel.navigateToMarketingNews = function () {
+        viewModel.navigateToNews(Marketing);
+    };
+
+    viewModel.navigateToDevNews = function () {
+        viewModel.navigateToNews(Dev);
+    };
+
+    viewModel.navigateToNews = function (category) {
+        viewModel.navigateToItems(category, NEWS);
+    };
+
+    viewModel.navigateToAllBlogPosts = function () {
+        viewModel.navigateToBlogPost(All);
+    };
+
+    viewModel.navigateToDesignBlogPosts = function () {
+        viewModel.navigateToBlogPost(Design);
+    };
+
+    viewModel.navigateToMarketingBlogPosts = function () {
+        viewModel.navigateToBlogPost(Marketing);
+    };
+
+    viewModel.navigateToDevBlogPosts = function () {
+        viewModel.navigateToBlogPost(Dev);
+    };
+
+    viewModel.navigateToBlogPost = function (category) {
+        viewModel.navigateToItems(category, BLOG_POSTS);
+    };
+
+    viewModel.navigateToItems = function (category, itemsType) {
+        var navigationEntry = {
+            moduleName: "views/itemsList/itemsList",
+            context: { items: itemsType, category: category },
+            animated: false
+        };
+
+        frames.topmost().navigate(navigationEntry);
+    };
+
+    viewModel.showMenu = function (args) {
+        var sideMenu = view.getViewById(viewModel.currentPage, mainMenuId);
+        if (sideMenu) {
+            sideMenu.showDrawer();
+        }
+    };
+
+    viewModel.navigateToPaidWhitePapers = function () {
+        var navigationEntry = {
+            animated: false
+        };
+
+        //if (TOKEN == null) {
+            navigationEntry.moduleName = "views/login/login";
+        //} else {
+        //    navigationEntry.moduleName = "views/whitePapers/whitePapers";
+        //}
+
+        frames.topmost().navigate(navigationEntry);
+    }
+
+    return viewModel;
+}
+
+
+//global.ServiceEndPoint = "http://10.0.3.2:89/";
+global.ServiceEndPoint = "http://192.168.152.142:4774/";
+global.ServiceEndPointWS = "http://192.168.152.142:4774";
+
+
+//global.NewsItemsServicePath = "CustomServices/ItemsService/GetNews/";
+global.NewsItemsServicePath = "api/quantum-mobile/newsitems";
+
+//global.BlogsServicePath = "CustomServices/ItemsService/GetBlogPosts/";
+global.BlogsServicePath = "api/quantum-mobile/blogposts";
+
+global.LogInServicePath = "api/quantum-mobile/login";
+
+global.WhitePapersServicePath = "api/quantum-mobile/documents";
+
+
+//http://localhost:89/api/mycustomservice/newsitems(eb57052e-9a43-66a7-8579-ff00003bc1d1)?$select=Title,Content,PublicationDate&sf_culture=es
+global.NewsItemServicePath = "api/quantum-mobile/newsitems({0})?$select=Title,Content,PublicationDate,RelatedMedia&sf_culture={1}";
+//global.NewsItemServicePath = "api/mycustomservice/blogposts({0})?$select=Title,Content,PublicationDate&sf_culture={1}";
+global.BlogServicePath = "api/quantum-mobile/blogposts({0})?$select=Title,Content,PublicationDate&sf_culture={1}";
+
+global.HierarchyTaxaPath = "api/quantum-mobile/hierarchy-taxa";
+
+global.NEWS = "news";
+global.BLOG_POSTS = "blogs";
+global.LANG = "EN";
+global.TOKEN = null;
+global.DesignCategoryId = null;
+global.MarketingCategoryId = null;
+global.DevelopmentCategoryId = null;
+
+
+application.resources["utcDateConverter"] = utcDateConverter;
+application.resources["utcDotNetDateConverter"] = utcDotNetDateConverter;
+
+application.start({ moduleName: "main-page" });
---
+++ app/views/item/item-model.js
@@ -0,0 +1,56 @@
+var Observable = require("data/observable").Observable;
+
+var frames = require("ui/frame");
+var http = require("http");
+
+function createViewModel() {
+    var viewModel = new Observable();
+
+    viewModel.getItem = function () {
+        var url = viewModel.resolveServiceURL();
+
+        http.request({ url: url, method: "GET" }).then(function (response) {
+            //http://10.0.2.2:89 or http://10.0.3.2:89 is your localhost because of the VM and the emulator
+            //E.g. http://10.0.3.2:89/api/mycustomservice/newsitems?$select=Id,Title,PublicationDate
+
+            var item = response.content.toJSON();
+
+            viewModel.set("title", item.Title);
+            viewModel.set("publicationDate", item.PublicationDate);
+            viewModel.set("content", item.Content);
+
+            if (item.RelatedMedia) {
+                viewModel.set("relatedMedia", item.RelatedMedia);
+            }
+        }, function (e) {
+            alert("Please review your endpoint. Error message: " + e);
+            console.log(e);
+        });
+    };
+
+    viewModel.refreshInES = function () {
+        viewModel.refreshUI("ES");
+    };
+
+    viewModel.refreshInEN = function () {
+        viewModel.refreshUI("EN");
+    };
+
+    viewModel.refreshUI = function (lang) {
+        LANG = lang;
+        viewModel.getItem();
+    };
+
+    viewModel.resolveServiceURL = function () {
+        var result = ServiceEndPoint;
+
+        result += viewModel.servicePath;
+        result = formatString(result, [viewModel.id, LANG]);
+
+        return result;
+    }
+
+    return viewModel;
+}
+
+exports.createViewModel = createViewModel;
